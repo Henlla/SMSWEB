56d3cadbe0970854daa11811a9d6daaf835e6170
Pham Kiet <84656881+Henlla@users.noreply.github.com>
2023-01-08 18:41:49 +0700

    Initial commit

---
+++ src/main/java/com/example/smsweb/SmswebApplication.java
@@ -0,0 +1,13 @@
+package com.example.smsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmswebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmswebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smsweb/SmswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmswebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
