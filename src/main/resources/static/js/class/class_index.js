5809c84bcc3841ae98769b5ca3c664128d1452c0
tinhtvt97 <85776875+tinhtvt97@users.noreply.github.com>
2023-04-02 13:48:20 +0700

    commit

--- src/main/resources/static/js/class/class_index.js
+++ src/main/resources/static/js/class/class_index.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('#reservationdate_u').datetimepicker({
         format: 'DD/MM/YYYY'
     });
+    var groupColumn = 1;
+    var count = 0;
     $("#class-table").DataTable({
         pageLength:5,
         lengthMenu:[[5,10,20,-1], [5, 10, 20,'All']],
@@ -32,13 +34,13 @@ $(document).ready(function () {
             }
         },initComplete: function () {
             count = 0;
-            this.api().columns([2]).every(function (i) {
+            this.api().columns([1]).every(function (i) {
                 var title = this.header();
                 //replace spaces with dashes
                 title = $(title).html().replace(/[\W]/g, '');
                 var column = this;
                 var select = $('<select id="' + title + '" class="form-control form-control-sm select2" style="width: 50%" ></select>')
-                    .appendTo($('#select-teacher'))
+                    .appendTo($('#select-department'))
                     .on('change', function () {
                         //Get the "text" property from each selected data
                         //regex escape the value and store in array
@@ -73,12 +75,55 @@ $(document).ready(function () {
                 $('.select2').val(null).trigger('change');
             });
         },
+        columnDefs: [{ visible: false, targets: groupColumn }],
+        order: [[groupColumn, 'asc']],
+        displayLength: 25,
+        drawCallback: function (settings) {
+            var api = this.api();
+            var rows = api.rows({ page: 'current' }).nodes();
+            var last = null;
+
+            api
+                .column(groupColumn, { page: 'current' })
+                .data()
+                .each(function (group, i) {
+                    if (last !== group) {
+                        $(rows)
+                            .eq(i)
+                            .before('<tr class="group group_'+count+' expand" onclick="groupItem('+count+')"><td class="text-bold text-lg text-uppercase" colspan="7">' + group + '</td></tr>');
+                        last = group;
+                        count++;
+                    }
+                });
+            count = 0;
+        },
     })
 
     $('.dataTables_filter input[type="search"]').css(
         {'width': '350px', 'display': 'inline-block'}
     );
+
+    // var group = $("tr[class='group']");
+    // group.click((event)=>{
+    //     console.log(event.target);
+    // })
 });
+ var groupItem = function (param) {
+     var class_list = $( ".group_"+param ).attr('class').split(' ');
+     console.log(class_list);
+     var bol = class_list.includes('expand');
+     console.log(bol)
+     if (bol){
+         $( ".group_"+param )
+             .nextUntil( ".group_"+(param+1) ).addClass("d-none");
+         $( ".group_"+param ).removeClass('expand')
+     }else {
+         $( ".group_"+param )
+             .nextUntil( ".group_"+(param+1) ).removeClass("d-none");
+         $( ".group_"+param ).addClass('expand')
+     }
+
+ }
 
 var OnUpdate = (id) => {
     $.ajax({
